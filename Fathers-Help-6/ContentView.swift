428c8404b0283690b51250323fe9d028608397d8
umpu <carol.ledgers0h@icloud.com>
2024-03-16 18:11:49 +0500

    Add CustomLayout

--- Fathers-Help-6/ContentView.swift
+++ Fathers-Help-6/ContentView.swift
@@ -7,18 +7,48 @@
 
 import SwiftUI
 
+struct CustomLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        let width = proposal.replacingUnspecifiedDimensions().width
+        let height = proposal.replacingUnspecifiedDimensions().height
+        let subviewsCount = CGFloat(subviews.count)
+        let viewHeight = height / subviewsCount
+        let viewSize = CGSize(width: viewHeight, height: viewHeight)
+        var positionX = bounds.minX
+        var positionY = bounds.maxY
+        
+        subviews.forEach { subview in
+            let position = CGPoint(x: positionX, y: positionY)
+            subview.place(at: position, anchor: .bottomLeading, proposal: ProposedViewSize(viewSize))
+            positionX += (width - viewHeight) / (subviewsCount - 1)
+            positionY -= viewHeight
+        }
+    }
+}
+
 struct ContentView: View {
     private var count = 7
+    @State var customMode = true
     
     var body: some View {
-        HStack {
+        let layout = customMode ? AnyLayout(HStackLayout()) : AnyLayout(CustomLayout())
+        
+        layout {
             ForEach(0..<count) { _ in
                 RoundedRectangle(cornerRadius: 10)
                     .aspectRatio(1.0, contentMode: .fit)
                     .foregroundStyle(.blue)
             }
         }
-        .ignoresSafeArea(edges: .horizontal)
+        .onTapGesture {
+            withAnimation() {
+                customMode.toggle()
+            }
+        }
     }
 }
 
